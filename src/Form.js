951ef8eaeb9b78c87dff9e82a563306f908867c5
speer987 <saarah.peer15@gmail.com>
2022-11-16 16:38:55 -0800

    adding new form and title.js files

---
+++ src/Form.js
@@ -0,0 +1,20 @@
+export default function Form({ action }) {
+  return (
+    <form id="dietFormGrid">
+      <p>Diet</p>
+      <input></input>
+      <p>Type</p>
+      <input></input>
+      <p>Max Ready Time</p>
+      <input></input>
+      <p>Title Match</p>
+      <input></input>
+      <p>Carbs</p>
+      <input></input>
+      <p>Protein</p>
+      <input></input>
+      <p>Allergies</p>
+      <input></input>
+    </form>
+  );
+}
---
+++ src/Title.js
@@ -0,0 +1,3 @@
+export default function Title({ text }) {
+  return <h1 id="centered">{text}</h1>;
+}
