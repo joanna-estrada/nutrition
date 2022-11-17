4fd51d2ca76a57914a8783e71a379c2cac03ba02
Joanna Estrada <jestra32@lion.lmu.edu>
2022-11-16 16:11:28 -0800

    11/16 Adding Some CSS

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,4 @@
+import './Home.css';
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -6,15 +7,28 @@ const Home = (props) => {
 
   return (
     <>
+    <div className = "Home">
+    <header className="Home-header">
       <h1>Nutrition App</h1>
       <p>Click a button to start!</p>
       <hr />
 
-      {/* Button */}
+      {/* Buttons */}
       <p>
         <button onClick={() => navigate("/dietForm")}>Go to Diet Form</button>
         <button onClick={() => navigate("/recipes")}>Go to Recipes</button>
+        <button onClick={() => navigate("/recipes")}>Get a Random Recipe</button>
       </p>
+      <a
+          className="App-link"
+          href="https://github.com/joanna-estrada/nutrition"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </header>
+      </div>
     </>
   );
 };
