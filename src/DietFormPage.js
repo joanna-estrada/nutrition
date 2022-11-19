3aa0acae1aab6df71c2d3ae3a6e26669672561f5
speer987 <saarah.peer15@gmail.com>
2022-11-18 20:11:33 -0800

    adding progress on Diet Form Page

--- src/DietFormPage.js
+++ src/DietFormPage.js
@@ -1,16 +1,14 @@
 import React from "react";
 import "./DietFormPage.css";
-import Title from "./Title.js";
 import Form from "./Form.js";
 import { useNavigate } from "react-router-dom";
 
 const DietForm = (props) => {
   const navigate = useNavigate();
   return (
-    <div>
-      <Title id="centeredText" text="Diet Form Page" />
+    <div id="grid-container">
       <Form />
-      <button onClick={() => navigate(-1)}>Go Back</button>
+      <button onClick={() => navigate(-1)}>← Go Back</button>
     </div>
   );
 };
--- src/Form.js
+++ src/Form.js
@@ -1,20 +1,50 @@
+import Title from "./Title.js";
+
 export default function Form({ action }) {
   return (
-    <form id="dietFormGrid">
-      <p>Diet</p>
-      <input></input>
-      <p>Type</p>
-      <input></input>
-      <p>Max Ready Time</p>
-      <input></input>
-      <p>Title Match</p>
-      <input></input>
-      <p>Carbs</p>
-      <input></input>
-      <p>Protein</p>
-      <input></input>
-      <p>Allergies</p>
-      <input></input>
+    <div id="form-container">
+    <Title id="centeredText" text="Diet Form Page" />
+    <form>
+      <p id="">Diet</p>
+      <input type="checkbox" id="gluten-free" name="gluten-free" value="gluten-free"></input>
+      <label for="gluten-free">Gluten Free</label><br></br>
+      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input>
+      <label for="vehicle1">Ketogenic</label><br></br>
+      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input>
+      <label for="vehicle1">Vegetarian</label><br></br>
+      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input>
+      <label for="vehicle1">Lacto-Vegetarian</label><br></br>
+      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input>
+      <label for="vehicle1">Ovovegetarian</label><br></br>
+      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input>
+      <label for="vehicle1">Vegan</label><br></br>
+      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input>
+      <label for="vehicle1">Pescetarian</label><br></br>
+      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input>
+      <label for="vehicle1">Paleo</label><br></br>
+      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input>
+      <label for="vehicle1">Primal</label><br></br>
+      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input>
+      <label for="vehicle1">Low FODMAP</label><br></br>
+      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input>
+      <label for="vehicle1">Whole30</label><br></br>
     </form>
+    <form>
+      
+      <input id=""></input>
+      <p id="">Type</p>
+      <input id=""></input>
+      <p id="">Max Ready Time</p>
+      <input id=""></input>
+      <p id="">Cuisine</p>
+      <input id=""></input>
+      <p id="">Carbs</p>
+      <input id=""></input>
+      <p id="">Protein</p>
+      <input id=""></input>
+      <p id="">Allergies</p>
+      <input id=""></input>
+    </form>
+    </div>
   );
 }
