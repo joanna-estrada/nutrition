2fce0bef809da3e74b57c3efded9d1c291d3c0ef
Joanna Estrada <jestra32@lion.lmu.edu>
2022-11-28 12:31:46 -0800

    Toal help 11/28

--- src/RanRecipesPage.js
+++ src/RanRecipesPage.js
@@ -1,13 +1,26 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
+import {useState, useEffect} from "react";
 
 const RanRecipesPage = (props) => {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const url = `https://api.spoonacular.com/recipes/random?apiKey=43dd31b79bdb404eacf941c2cdf9e291`;
+    console.log(url);
+    fetch(url)
+      .then((r) => r.json())
+      .then((r) => setData(r))
+      .catch((e) => setData(e));
+  }, []);
+
   const navigate = useNavigate();
   return (
     <>
       <h1>Random Recipes Page</h1>
       <br />
+      {data?.recipes?.[0]?.title}
       <button onClick={() => navigate(-1)}>Go Back</button>
+
     </>
   );
 };
