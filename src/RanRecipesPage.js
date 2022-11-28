4d126ae02e6cf4211d9d02f8c3cd7eeb48520654
speer987 <saarah.peer15@gmail.com>
2022-11-28 13:08:42 -0800

    adding some divs to the random recipes page

--- src/RanRecipesPage.js
+++ src/RanRecipesPage.js
@@ -1,11 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import {useState, useEffect} from "react";
+import "./App.css";
 
 const RanRecipesPage = (props) => {
   const [data, setData] = useState("");
 
   useEffect(() => {
-    const url = `https://api.spoonacular.com/recipes/random?apiKey=43dd31b79bdb404eacf941c2cdf9e291`;
+    const url = `https://api.spoonacular.com/recipes/random?apiKey=43dd31b79bdb404eacf941c2cdf9e291&number=3`;
     console.log(url);
     fetch(url)
       .then((r) => r.json())
@@ -18,7 +19,15 @@ const RanRecipesPage = (props) => {
     <>
       <h1>Random Recipes Page</h1>
       <br />
-      {data?.recipes?.[0]?.title}
+      <div className="randContainer">
+        {data?.recipes?.[0]?.title}
+      </div>
+      <div className="randContainer">
+        {data?.recipes?.[1]?.title}
+      </div>
+      <div className="randContainer">
+        {data?.recipes?.[2]?.title}
+      </div>
       <button onClick={() => navigate(-1)}>Go Back</button>
 
     </>
