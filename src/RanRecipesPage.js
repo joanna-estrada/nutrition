5e56bfc0317718e3c510522ca65fcc13f1b31c59
speer987 <saarah.peer15@gmail.com>
2022-12-01 14:29:11 -0800

    adding updates to random recipes page with save to favorites button

--- src/RanRecipesPage.js
+++ src/RanRecipesPage.js
@@ -26,41 +26,49 @@ const RanRecipesPage = (props) => {
         <div className="rand-container">
           <subtitle>{data?.recipes?.[0]?.title}</subtitle>
           <img src={data?.recipes?.[0]?.image}></img>
+          <button onClick={data?.recipes?.[0]?.sourceUrl} className="info-button">Go to Recipe</button>
+          <button onClick="" className="favorite-button">Save to Favorites</button>
           <subtitle>Summary</subtitle>
-          <div className="summary" dangerouslySetInnerHTML={{__html : firstRecipeSummary}}/>
+          <p dangerouslySetInnerHTML={{__html : firstRecipeSummary}}/>
+          <p className="warning">Note: Some of the recipe links may not lead to a recipe because Spoonacular has removed them.</p>
           <subtitle>Servings</subtitle>
           <p>{data?.recipes?.[0]?.servings}</p>
           <subtitle>Ready In</subtitle>
           <p>{data?.recipes?.[0]?.readyInMinutes} minutes</p>
           <subtitle>Nutrition Label</subtitle>
           <img src={`https://api.spoonacular.com/recipes/${data?.recipes?.[0]?.id}/nutritionLabel.png?apiKey=43dd31b79bdb404eacf941c2cdf9e291`}></img>
-          <button onClick={data?.recipes?.[0]?.sourceUrl} className="info-button">Go to Recipe</button>
         </div>
         <div className="rand-container">
           <subtitle>{data?.recipes?.[1]?.title}</subtitle>
           <img src={data?.recipes?.[1]?.image}></img>
+          <button onClick={data?.recipes?.[1]?.sourceUrl} className="info-button">Go to Recipe</button>
+          <button onClick="" className="favorite-button">Save to Favorites</button>
           <subtitle>Summary</subtitle>
-          <div className="summary" dangerouslySetInnerHTML={{__html : secondRecipeSummary}}/>
+          <p dangerouslySetInnerHTML={{__html : secondRecipeSummary}}/>
+          <p className="warning">Note: Some of the recipe links may not lead to a recipe because Spoonacular has removed them.</p>
           <subtitle>Servings</subtitle>
           <p>{data?.recipes?.[1]?.servings}</p>
           <subtitle>Ready In</subtitle>
           <p>{data?.recipes?.[1]?.readyInMinutes} minutes</p>
           <subtitle>Nutrition Label</subtitle>
           <img src={`https://api.spoonacular.com/recipes/${data?.recipes?.[1]?.id}/nutritionLabel.png?apiKey=43dd31b79bdb404eacf941c2cdf9e291`}></img>
-          <button onClick={data?.recipes?.[1]?.sourceUrl} className="info-button">Go to Recipe</button>
         </div>
         <div className="rand-container">
           <subtitle>{data?.recipes?.[2]?.title}</subtitle>
           <img src={data?.recipes?.[2]?.image}></img>
+          <button onClick={data?.recipes?.[2]?.sourceUrl} className="info-button">Go to Recipe</button>
+          <button onClick="" className="favorite-button">Save to Favorites</button>
+          <br/>
           <subtitle>Summary</subtitle>
-          <div className="summary" dangerouslySetInnerHTML={{__html : thirdRecipeSummary}}/>
+          <p dangerouslySetInnerHTML={{__html : thirdRecipeSummary}}/>
+          <p className="warning">Note: Some of the recipe links may not lead to a recipe because Spoonacular has removed them.</p>
           <subtitle>Servings</subtitle>
           <p>{data?.recipes?.[2]?.servings}</p>
           <subtitle>Ready In</subtitle>
           <p>{data?.recipes?.[2]?.readyInMinutes} minutes</p>
           <subtitle>Nutrition Label</subtitle>
           <img src={`https://api.spoonacular.com/recipes/${data?.recipes?.[2]?.id}/nutritionLabel.png?apiKey=43dd31b79bdb404eacf941c2cdf9e291`}></img>
-          <button onClick={data?.recipes?.[2]?.sourceUrl} className="info-button">Go to Recipe</button>
+          
         </div>
       </div>
       <button onClick={() => navigate(-1)}>Go Back</button>
