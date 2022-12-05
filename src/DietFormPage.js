bcd20587270c58799c0bd377460cd67da4a69926
speer987 <saarah.peer15@gmail.com>
2022-12-05 12:57:07 -0800

    adding go back and new search buttons

--- src/DietFormPage.js
+++ src/DietFormPage.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./DietFormPage.css";
 import Form from "./Form.js";
 import NavigationButton from "./components/NavigationButton";
+import SearchButton from "./components/SearchButton";
 
 const DietForm = (props) => {
   return (
     <div>
       <Form />
       <NavigationButton className="nav-button"/>
+      <SearchButton className="search-button" />
     </div>
   );
 };
