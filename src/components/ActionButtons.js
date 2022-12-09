a5d689ea8920e6ab439befa8a5a414119251db28
speer987 <saarah.peer15@gmail.com>
2022-12-09 14:53:24 -0800

    adding changes to favorite and info button on random recipe page

--- src/components/ActionButtons.js
+++ src/components/ActionButtons.js
@@ -3,10 +3,10 @@ export default function ActionButtons({recipe}) {
     <div id="align-buttons">
         {/* With help from: https://stackoverflow.com/a/2906586 */}
         <form action={recipe?.sourceUrl} >
-            <input type="submit" value="Go To Recipe" target="_blank"/>
+            <input className="action-buttons" type="submit" value="Go To Recipe" target="_blank"/>
         </form>
         <form action="#">
-            <input type="submit" value="Favorite Recipe"/>
+            <input className="action-buttons" type="submit" value="Favorite Recipe"/>
         </form>
     </div>
   );
