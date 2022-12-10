45b9cf2b7e58a44999691e148229c041ff97cf27
speer987 <saarah.peer15@gmail.com>
2022-12-10 10:24:48 -0800

    adding changes to favorite button so it opens in a new tab and changes to css

--- src/components/ActionButtons.js
+++ src/components/ActionButtons.js
@@ -2,8 +2,8 @@ export default function ActionButtons({recipe}) {
   return (
     <div id="align-buttons">
         {/* With help from: https://stackoverflow.com/a/2906586 */}
-        <form action={recipe?.sourceUrl} >
-            <input className="action-buttons" type="submit" value="Go To Recipe" target="_blank"/>
+        <form action={recipe?.sourceUrl} target="_blank">
+            <input className="action-buttons" type="submit" value="Go To Recipe"/>
         </form>
         <form action="#">
             <input className="action-buttons" type="submit" value="Favorite Recipe"/>
