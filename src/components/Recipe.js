d42d02e2f1dac481c7ad9bd191c52e18b78b424a
speer987 <saarah.peer15@gmail.com>
2022-12-10 10:20:17 -0800

    adding favorite and save recipe button to diet form page

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,12 +1,14 @@
 import SmallSubtitle from "./SmallSubtitle";
 import Summary from "./Summary";
 import Image from "./Image";
+import ActionButtons from "./ActionButtons";
 
 export default function Recipe({id, recipe, extraRecipeData}) {
   return(
     <div id={id}>
         <SmallSubtitle text={recipe?.title} />
         <Image src={recipe?.image} alt="recipe image" />
+        <ActionButtons recipe={extraRecipeData} />
         <SmallSubtitle text="Summary" />
         <Summary recipe={extraRecipeData} />
         <SmallSubtitle text="Servings" />
